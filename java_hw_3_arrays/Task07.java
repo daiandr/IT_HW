d647f7ae9bdb0c5e63ad65d19be53898e35d5d2a
daiandr <dayan.dragomirov@gmail.com>
2015-11-12 02:29:03 +0200

    Added hw3_Task07

--- java_hw_3_arrays/Task07.java
+++ java_hw_3_arrays/Task07.java
@@ -1,4 +1,21 @@
 
 public class Task7 {
+	public static void main(String[] args) {
+
+		int[] arr = { 21, 3, 4, 24, 45, 32, 71, 12 };
+
+		int[] newArr = new int[arr.length];
+
+		newArr[0] = arr[1];
+		newArr[newArr.length - 1] = arr[arr.length - 2];
+
+		for (int index = 1; index < newArr.length - 1; index++) {
+			newArr[index] = arr[index - 1] + arr[index + 1];
+		}
+		for (int i = 0; i < newArr.length; i++) {
+			System.out.print(newArr[i] + ",");
+		}
+
+	}
 
 }
